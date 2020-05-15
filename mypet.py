c3c01b4406cf64e981fd2e557c0ce3742fc961a9
yifanliuu <kokonoseren@163.com>
2020-05-15 23:29:16 +0800

    feat: 实现动画效果

--- mypet.py
+++ mypet.py
@@ -10,15 +10,16 @@ from threading import Thread
 
 class NewWindow(QWidget):
     def __init__(self, parent=None, **kwargs):
-        self.i = 1
         super(NewWindow, self).__init__(parent)
+        self.i = 1
+        self.mypix_init()
         self.move(1200, 600)
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
         self.setAutoFillBackground(False)
         self.setAttribute(Qt.WA_TranslucentBackground, True)
         self.timer = QTimer()
-        self.timer.setInterval(500)
-        self.mypix()
+        self.timer.setInterval(1000)
+        self.timer.timeout.connect(self.time_changed)
         self.timer.start()
         self.initUI()
 
@@ -35,13 +36,28 @@ class NewWindow(QWidget):
         t2 = Thread(target=self.initUI())
         t2.start()
 
-    def mypix(self):
+    def mypix_init(self):
         self.pix = QPixmap('./img/shime1.png', '0', Qt.AvoidDither |
                            Qt.ThresholdAlphaDither | Qt.ThresholdDither)
         self.resize(self.pix.size())
         self.setMask(self.pix.mask())
-        self.img = QLabel(self)
-        self.img.setPixmap(self.pix)
+
+    def mypix(self):
+        self.update()
+        if self.i == 47:
+            self.i = 1
+        self.mypic = {}
+        for i in range(1, 47):
+            self.mypic[i] = './img/shime'+str(i) + '.png'
+        self.pix = QPixmap(self.mypic[self.i], '0', Qt.AvoidDither |
+                           Qt.ThresholdAlphaDither | Qt.ThresholdDither)
+        self.resize(self.pix.size())
+        self.setMask(self.pix.mask())
+        self.dragPosition = None
+
+    def paintEvent(self, QPaintEvent):
+        painter = QPainter(self)
+        painter.drawPixmap(0, 0, self.pix.width(), self.pix.height(), self.pix)
 
 
 if __name__ == '__main__':
